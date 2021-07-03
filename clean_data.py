e338dae342413084b319d713d3cb2162176238a5
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-07-03 19:15:35 +1000

    factorisation and bug fixes

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,18 @@
+import pickle
+
 import pandas as pd
+import numpy as np
 from tqdm import tqdm
+from joblib import dump, load
+
+
+def setting_index(df):
+    """
+    Returns a sorted datetime index
+    """
+    df['Quarter end'] = pd.to_datetime(df['Quarter end'])
+    df = df.set_index("Quarter end")
+    return df.sort_index(ascending=False)
 
 
 def class_creation(df, threshold=3):
@@ -24,69 +37,76 @@ def class_creation(df, threshold=3):
         return 2
 
 
-stocks_df = pd.read_csv('historical_qrs.csv', index_col=['Ticker', 'Row ID'])
+# stocks_df = pd.read_csv('historical_qrs.csv', index_col=['Ticker', 'Row ID'])
+#
+# df_by_ticker = {}
+# for ticker in tqdm(stocks_df.index.get_level_values('Ticker')):
+#     df_by_ticker[ticker] = stocks_df.loc[ticker, :]
 
 """
 Idea:
 Takes a five year period of QRs as input and outputs buy, sell or hold for next quarter
 - if a company has been traded for less than five years, missing data is replaced with nan (top padded)
 - assumes future performance is not always independent from the past (rejects random walk hypothesis)
-- takes relative change as input but also considers relative total assets (not the change thereof)
-- assumes relative size of a company may matter too 
+# - takes relative change as input but also considers relative total assets (not the change thereof)
+# - assumes relative size of a company may matter too 
 - if there exists data for a company over a period of N quarters than N-2 different training data can be generated 
 using a sliding window method 
 """
 
-print()
-# # Setting the index as the Date
-# for i in tqdm(stocks_df.keys()):
-#     stocks_df[i] = setting_index(stocks_df[i])
-#
-# # Replacing all "None" values with NaN
-# for i in tqdm(stocks_df.keys()):
-#     stocks_df[i].replace("None", 0, inplace=True)
-#
-# # Creating a new dictionary that contains the numerical values, then converting all values to numeric values
-# num_df = {}
-# for i in tqdm(stocks_df.keys()):
-#     num_df[i] = stocks_df[i].apply(pd.to_numeric)
-#
-# # Replacing values with percent difference or change
-# pcnt_df = {}
-# for i in tqdm(num_df.keys()):
-#     pcnt_df[i] = num_df[i].pct_change(periods=1).apply(lambda x: x * 100)
-#
-# # Replacing infinite values with NaN
-# for i in tqdm(pcnt_df.keys()):
-#     pcnt_df[i] = pcnt_df[i].replace([np.inf, -np.inf], np.nan)
-#
-# # Creating a new DataFrame that contains the class 'Decision' determining if a quarterly reports improvement is a buy, hold, or sell.
-# new_df = {}
-# for i in tqdm(pcnt_df.keys()):
-#     # Assigning the new DF
-#     new_df[i] = pcnt_df[i]
-#
-#     # Creating the new column with the classes, shifted by -1 in order to know if the prices will increase/decrease in the next quarter.
-#     new_df[i]['Decision'] = new_df[i].apply(class_creation, axis=1).shift(-1)
-#
-# # Excluding the first and last rows
-# for i in tqdm(new_df.keys()):
-#     new_df[i] = new_df[i][1:-1]
-#
-# # Combining all stock DFs into one
-# big_df = pd.DataFrame()
-# for i in tqdm(pcnt_df.keys()):
-#     big_df = big_df.append(new_df[i], sort=False)
-#
-# # Filling the NaNs with 0
-# big_df.fillna(0, inplace=True)
-#
-# # Resetting the index because we no longer need the dates
-# big_df.reset_index(drop=True, inplace=True)
-#
-# # Dropping the price related columns to prevent data leakage
-# big_df.drop(['Price', 'Price high', 'Price low'], 1, inplace=True)
-#
-# # Exporting the final DataFrame
-# with open("main_df.pkl", 'wb') as fp:
-#     pickle.dump(big_df, fp)
+qr_by_ticker = load('qr_by_ticker.joblib')
+
+# Setting the index as the Date
+for i in tqdm(qr_by_ticker.keys()):
+    qr_by_ticker[i] = setting_index(qr_by_ticker[i])
+
+# Replacing all "None" values with NaN
+for i in tqdm(qr_by_ticker.keys()):
+    qr_by_ticker[i].replace("None", np.nan, inplace=True)
+
+# Creating a new dictionary that contains the numerical values, then converting all values to numeric values
+num_df = {}
+for i in tqdm(qr_by_ticker.keys()):
+    num_df[i] = qr_by_ticker[i].apply(pd.to_numeric)
+
+# Replacing values with percent difference or change
+pcnt_df = {}
+for i in tqdm(num_df.keys()):
+    pcnt_df[i] = num_df[i].pct_change(periods=1)
+
+# Replacing infinite values with NaN
+for i in tqdm(pcnt_df.keys()):
+    pcnt_df[i] = pcnt_df[i].replace([np.inf, -np.inf], np.nan)
+
+# Creating a new DataFrame that contains the class 'Decision' determining if a quarterly reports improvement is a
+# buy, hold, or sell.
+new_df = {}
+for i in tqdm(pcnt_df.keys()):
+    # Assigning the new DF
+    new_df[i] = pcnt_df[i]
+
+    # Creating the new column with the classes, shifted by -1 in order to know if the prices will increase/decrease in
+    # the next quarter.
+    new_df[i]['Decision'] = new_df[i].apply(class_creation, axis=1).shift(-1)
+
+# Excluding the first and last rows
+for i in tqdm(new_df.keys()):
+    new_df[i] = new_df[i][1:-1]
+
+# Combining all stock DFs into one
+big_df = pd.DataFrame()
+for i in tqdm(pcnt_df.keys()):
+    big_df = big_df.append(new_df[i], sort=False)
+
+# Filling the NaNs with 0
+big_df.fillna(0, inplace=True)
+
+# Resetting the index because we no longer need the dates
+big_df.reset_index(drop=True, inplace=True)
+
+# Dropping the price related columns to prevent data leakage
+big_df.drop(['Price', 'Price high', 'Price low'], 1, inplace=True)
+
+# Exporting the final DataFrame
+with open("main_df.pkl", 'wb') as fp:
+    pickle.dump(big_df, fp)
--- download.py
+++ download.py
@@ -5,20 +5,25 @@ from io import StringIO
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from tqdm import tqdm
+from joblib import dump
 
 
 html_page = urlopen('https://web.archive.org/web/20200123214339/http://www.stockpup.com/data')
 soup = BeautifulSoup(html_page)
-qrs = []
-tickers = []
+qr_by_tickers = {}
 
-for tag in tqdm(soup.findAll('a', attrs={'href': re.compile('^/web/'), 'title': re.compile(r'\.csv$')})):
+for tag in tqdm(soup.findAll('a', attrs={'href': re.compile('^/web/'), 'title': re.compile(r'\.csv$')})[-3:]):
     url = 'https://web.archive.org/' + tag.get('href')
     data = StringIO(requests.get(url).text)
-    qrs.append(pd.read_csv(data))
 
-    tickers.append(re.search(r'^fundamental_data_excel_(.*)\.csv$', tag.get('title'), re.IGNORECASE).group(1))
+    ticker = re.search(r'^fundamental_data_excel_(.*)\.csv$', tag.get('title'), re.IGNORECASE).group(1)
 
-historical_qrs = pd.concat(qrs, keys=tickers, names=['Ticker', 'Row ID'])
-historical_qrs.to_csv('historical_qrs.csv', index=True)
-print()
+    qr = pd.read_csv(data)
+    qr_by_tickers[ticker] = qr
+
+# historical_qrs = pd.concat(qrs, keys=tickers, names=['Ticker', 'Row ID'])
+# remove html tag column
+# del historical_qrs[historical_qrs.columns[-1]]
+# historical_qrs.to_csv('historical_qrs.csv', index=True)
+
+dump(qr_by_tickers, 'qr_by_tickers.joblib')
