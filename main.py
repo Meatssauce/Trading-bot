450f633cd8952ca2f82696e6df0314af09b0abc6
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-07-14 17:00:07 +1000

    bug fixes

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def make_model(sequence_length, feature_count):
 
 # Load data
 with open('data/X.pkl', 'rb') as X_file, open('data/y.pkl', 'rb') as y_file:
-    X_by_tickers, y_by_tickers = load(X_file), load(y_file)
+    X, y = load(X_file), load(y_file)
 y = to_categorical(y, 3)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
